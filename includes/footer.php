38227449688d2d24b415f1380b361347a68164da
seba5879 <seba5879@edu.easj.dk>
2021-02-02 12:57:02 +0100

    git init

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+    <div>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
